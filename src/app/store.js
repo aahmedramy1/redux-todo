ee5e3b1644c26ab9230759348cb382d22cd48b3c
Ahmed Ramy <ahmedramy.e404@gmail.com>
2021-08-29 19:47:43 +0200

    Added input component and initiated redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "../features/todoSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: { todos: rootReducer },
 });
---
+++ src/components/TodoItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoItem = ({ name, done, id }) => {
+  return <div className="todoItem"></div>;
+};
+
+export default TodoItem;
